065a25d96fe38b2e44dc0b58625cf85f63d897c4
khmgobe <dr94406@naver.com>
2025-01-20 16:10:30 +0900

    init : 프로젝트 생성.

---
+++ src/main/java/io/backend/assignment/BackEndAssignmentApplication.java
@@ -0,0 +1,13 @@
+package io.backend.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/backend/assignment/BackEndAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.backend.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
