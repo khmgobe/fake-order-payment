c00f680e39f8ea238ab6bf6293e8b91c5459d527
khmgobe <dr94406@naver.com>
2025-01-20 18:00:16 +0900

    6. feat: Product 도메인 객체 생성 및 생성자, 생성자 검증 코드 추가.

--- src/test/java/io/backend/assignment/feature/ProductServiceTest.java
+++ src/test/java/io/backend/assignment/feature/ProductServiceTest.java
@@ -39,7 +39,7 @@ class ProductServiceTest {
 
     private class RegisterProduct {
         public void request(ProductRequest request) {
-            request.toDomain();
+            request.toDomain(request);
         }
     }
 
@@ -51,20 +51,63 @@ class ProductServiceTest {
             LocalDateTime create_at,
             LocalDateTime update_at) {
 
-        ProductRequest{
+        ProductRequest {
             Assert.hasText(name, "이름은 필수입니다.");
-            Assert.notNull(price,"상품 가격은 필수입니다." );
+            Assert.notNull(price, "상품 가격은 필수입니다.");
             Assert.notNull(stock, "수량은 필수입니다.");
             Assert.notNull(create_at, "생성 시간은 필수입니다.");
             Assert.notNull(update_at, "수정 시간은 필수입니다.");
         }
 
-        public Product toDomain() {
-            return null;
+        public Product toDomain(ProductRequest request) {
+            return new Product(
+                    request.name(),
+                    request.description(),
+                    request.price(),
+                    request.stock(),
+                    request.create_at(),
+                    request.update_at());
         }
     }
 
-    private class Product {
+    private static class Product {
+
+        private Long id;
+        private final String name;
+        private final String description;
+        private final Long price;
+        private final Integer stock;
+        private final LocalDateTime create_at;
+        private final LocalDateTime update_at;
+
+        public Product(
+                final String name,
+                final String description,
+                final Long price,
+                final Integer stock,
+                final LocalDateTime create_at,
+                final LocalDateTime update_at) {
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.stock = stock;
+            this.create_at = create_at;
+            this.update_at = update_at;
+            validateConstructor(name, price, stock, create_at, update_at);
+        }
+
+        private void validateConstructor(
+                final String name,
+                final Long price,
+                final Integer stock,
+                final LocalDateTime create_at,
+                final LocalDateTime update_at) {
+            Assert.hasText(name, "상품 이름은 필수입니다.");
+            Assert.notNull(price, "상품 가격은 필수입니다.");
+            Assert.notNull(stock, "재고 수량은 필수입니다.");
+            Assert.notNull(create_at, "생성 시간은 필수입니다.");
+            Assert.notNull(update_at, "수정 시간은 필수입니다.");
+        }
     }
 }
 
