3a7f546bc1e5cacbec220012862b1cd08419d279
khmgobe <dr94406@naver.com>
2025-01-20 18:15:55 +0900

    10. feat: Repository를 POJO 코드로 테스트한다.

--- src/main/java/io/backend/assignment/domain/ProductRepository.java
+++ src/main/java/io/backend/assignment/domain/ProductRepository.java
@@ -1,16 +1,24 @@
 package io.backend.assignment.domain;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Component
 public class ProductRepository {
-    private final Map<Long, ProductServiceTest.Product> products = new HashMap<>();
+    private final Map<Long, Product> products = new HashMap<>();
     private Long productId = 1L;
 
-
-    public void save(final ProductServiceTest.Product product) {
+    public void save(final Product product) {
 
         product.assignId(productId++);
         products.put(product.getId(), product);
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
     }
 }
--- src/test/java/io/backend/assignment/feature/ProductServiceTest.java
+++ src/test/java/io/backend/assignment/feature/ProductServiceTest.java
@@ -1,22 +1,22 @@
 package io.backend.assignment.feature;
 
+import io.backend.assignment.domain.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductServiceTest {
-
 
     private RegisterProduct registerProduct;
     private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
+        productRepository = new ProductRepository();
         registerProduct = new RegisterProduct(productRepository);
     }
 
@@ -39,107 +39,9 @@ class ProductServiceTest {
         RegisterProduct.ProductRequest request = new RegisterProduct.ProductRequest(name, description, price, stock, create_at, update_at);
         registerProduct.register(request);
 
+        assertThat(productRepository.findAll().size()).isEqualTo(1);
+
     }
 
-    private class RegisterProduct {
-
-        private final ProductRepository productRepository;
-
-        private RegisterProduct(final ProductRepository productRepository) {
-            this.productRepository = productRepository;
-        }
-
-        public void register(ProductRequest request) {
-            final Product product = request.toDomain(request);
-            productRepository.save(product);
-        }
-
-        private record ProductRequest(
-                String name,
-                String description,
-                Long price,
-                Integer stock,
-                LocalDateTime create_at,
-                LocalDateTime update_at) {
-
-            ProductRequest {
-                Assert.hasText(name, "이름은 필수입니다.");
-                Assert.notNull(price, "상품 가격은 필수입니다.");
-                Assert.notNull(stock, "수량은 필수입니다.");
-                Assert.notNull(create_at, "생성 시간은 필수입니다.");
-                Assert.notNull(update_at, "수정 시간은 필수입니다.");
-            }
-
-            public Product toDomain(ProductRequest request) {
-                return new Product(
-                        request.name(),
-                        request.description(),
-                        request.price(),
-                        request.stock(),
-                        request.create_at(),
-                        request.update_at());
-            }
-        }
-    }
-
-    private static class Product {
-
-        private Long id;
-        private final String name;
-        private final String description;
-        private final Long price;
-        private final Integer stock;
-        private final LocalDateTime create_at;
-        private final LocalDateTime update_at;
-
-        public Product(
-                final String name,
-                final String description,
-                final Long price,
-                final Integer stock,
-                final LocalDateTime create_at,
-                final LocalDateTime update_at) {
-            this.name = name;
-            this.description = description;
-            this.price = price;
-            this.stock = stock;
-            this.create_at = create_at;
-            this.update_at = update_at;
-            validateConstructor(name, price, stock, create_at, update_at);
-        }
-
-        private void validateConstructor(
-                final String name,
-                final Long price,
-                final Integer stock,
-                final LocalDateTime create_at,
-                final LocalDateTime update_at) {
-            Assert.hasText(name, "상품 이름은 필수입니다.");
-            Assert.notNull(price, "상품 가격은 필수입니다.");
-            Assert.notNull(stock, "재고 수량은 필수입니다.");
-            Assert.notNull(create_at, "생성 시간은 필수입니다.");
-            Assert.notNull(update_at, "수정 시간은 필수입니다.");
-        }
-
-        public void assignId(final Long id) {
-            this.id = id;
-        }
-
-        public Long getId() {
-            return id;
-        }
-    }
-
-    private class ProductRepository {
-        private final Map<Long, Product> products = new HashMap<>();
-        private Long productId = 1L;
-
-
-        public void save(final Product product) {
-
-            product.assignId(productId++);
-            products.put(product.getId(), product);
-        }
-    }
 }
 
