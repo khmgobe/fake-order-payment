d23cb57ac833120bb47408b40275e44859e2a0c0
khmgobe <dr94406@naver.com>
2025-01-20 17:52:56 +0900

    4. feat: (Test 클래스 생성, Register 클래스 및 ProductRequest 레코드 생성)

---
+++ src/test/java/io/backend/assignment/feature/ProductServiceTest.java
@@ -0,0 +1,61 @@
+package io.backend.assignment.feature;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+
+class ProductServiceTest {
+
+
+    private RegisterProduct registerProduct;
+
+    @BeforeEach
+    void setUp() {
+        registerProduct = new RegisterProduct();
+    }
+
+    /**
+     * 1. 상품을 등록한다. []
+     * 2. 상품 목록을 조회한다. []
+     * 3. 상품 목록이 비어있을 경우, 예외를 발생시킨다. []
+     */
+    @Test
+    @DisplayName("상품을 조회한다.")
+    void findProduct() {
+
+        final String name = "name";
+        final String description = "description";
+        final long price = 10000L;
+        final int stock = 2;
+        final LocalDateTime create_at = LocalDateTime.now();
+        final LocalDateTime update_at = LocalDateTime.now();
+
+        ProductRequest productRequest = new ProductRequest(name, description, price, stock, create_at, update_at);
+
+    }
+
+    private class RegisterProduct {
+
+    }
+
+    private record ProductRequest(
+            String name,
+            String description,
+            Long price,
+            Integer stock,
+            LocalDateTime create_at,
+            LocalDateTime update_at) {
+
+        ProductRequest{
+            Assert.hasText(name, "이름은 필수입니다.");
+            Assert.notNull(price,"상품 가격은 필수입니다." );
+            Assert.notNull(stock, "수량은 필수입니다.");
+            Assert.notNull(create_at, "생성 시간은 필수입니다.");
+            Assert.notNull(update_at, "수정 시간은 필수입니다.");
+        }
+    }
+}
+
