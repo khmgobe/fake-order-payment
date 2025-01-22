03bee9c2c9d46389368b98514258bfd177700e3c
khmgobe <dr94406@naver.com>
2025-01-22 14:38:06 +0900

    17. chore: 테스트 내 코드 분리와 설명 추가, 사용하지 않는 코드 삭제.

--- src/test/java/io/backend/assignment/feature/ProductServiceTest.java
+++ src/test/java/io/backend/assignment/feature/ProductServiceTest.java
@@ -3,7 +3,6 @@ package io.backend.assignment.feature;
 import io.backend.assignment.common.ApiTest;
 import io.backend.assignment.common.TestScenario;
 import io.backend.assignment.service.ProductService;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +14,6 @@ class ProductServiceTest extends ApiTest {
     @Autowired
     private ProductService productService;
     
-    @Autowired
-    private EntityManager entityManager;
-
     /**
      * 1. 상품을 등록한다. [O]
      * 2. 구매 가능한 상품 목록을 조회한다. [O]
@@ -26,14 +22,32 @@ class ProductServiceTest extends ApiTest {
     @Test
     @DisplayName("상품을 등록하고 구매 가능한 상품을 조회한다. [정상 케이스]")
     void findAllAvailableProducts() {
+
+        // given : 상품 등록
         TestScenario.registerProductApi().request();
-        assertThat(productService.findAllProducts().size()).isEqualTo(1);
+
+        // when : 구매 가능한 상품의 재고 조회
+        int productCount = productService.findAllProducts().size();
+
+        // then : 상품이 정상적으로 조회되어야 한다.
+        assertThat(productCount)
+                .as("재고가 0개 이상인 상품이 있어야 합니다.")
+                .isEqualTo(1);
     }
 
     @Test
     @DisplayName("상품을 등록하고 재고가 0인 상품을 조회한다. [실패 케이스]")
     void findAllUnAvailableProducts() {
+
+        // given : 재고가 0인 상품 상품 등록
         TestScenario.registerProductApi().stock(0).request();
-        assertThat(productService.findAllProducts().size()).isEqualTo(0);
+
+        // when : 구매 가능한 상품의 재고 조회
+        int productCount = productService.findAllProducts().size();
+
+        // then : 상품이 조회되지 않아야 한다.
+        assertThat(productCount)
+                .as("재고가 0인 상품은 조회되지 않아야 합니다.")
+                .isEqualTo(0);
     }
 }
