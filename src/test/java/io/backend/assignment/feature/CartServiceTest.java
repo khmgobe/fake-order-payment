8a8a4817b07a45597eb0c221a21601a84f26a3a4
khmgobe <dr94406@naver.com>
2025-01-22 17:42:29 +0900

    1. feat: CartServiceTest 클래스 작성 및 addProductToCart 메서드 생성.

---
+++ src/test/java/io/backend/assignment/feature/CartServiceTest.java
@@ -0,0 +1,14 @@
+package io.backend.assignment.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CartServiceTest {
+
+
+    @Test
+    @DisplayName("상품을 장바구니에 추가한다. [정상 케이스]")
+    void addProductToCart()  {
+
+    }
+}
