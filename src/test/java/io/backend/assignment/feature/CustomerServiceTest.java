cbd7804546f2bd840c189f9859463d23ced58ebc
khmgobe <dr94406@naver.com>
2025-01-22 14:52:07 +0900

    1. feat: CustomerServiceTest 생성 및 RegisterCustomer 내부클래스 작성.

---
+++ src/test/java/io/backend/assignment/feature/CustomerServiceTest.java
@@ -0,0 +1,18 @@
+package io.backend.assignment.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CustomerServiceTest {
+
+
+    @Test
+    @DisplayName("사용자를 등록하고 조회한다.")
+    void registerCustomer()  {
+
+        RegisterCustomer registerCustomer = new RegisterCustomer();
+    }
+
+    private class RegisterCustomer {
+    }
+}
