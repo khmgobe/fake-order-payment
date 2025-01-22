86038d12fe7311f058c7e5ad1084c51a70e2a44e
khmgobe <dr94406@naver.com>
2025-01-22 14:59:30 +0900

    2. feat: request에 필요한 데이터 세팅 및 생성자 밸리데이션 추가.

--- src/test/java/io/backend/assignment/feature/CustomerServiceTest.java
+++ src/test/java/io/backend/assignment/feature/CustomerServiceTest.java
@@ -1,18 +1,51 @@
 package io.backend.assignment.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
 
 class CustomerServiceTest {
 
+
+    private RegisterCustomer registerCustomer;
+
+    @BeforeEach
+    void setUp() {
+        registerCustomer = new RegisterCustomer();
+    }
 
     @Test
     @DisplayName("사용자를 등록하고 조회한다.")
     void registerCustomer()  {
 
-        RegisterCustomer registerCustomer = new RegisterCustomer();
+        final Long id = 1L;
+        final String name = "name";
+        final LocalDateTime create_at = LocalDateTime.now();
+        final LocalDateTime update_at = LocalDateTime.now();
+
+        RegisterCustomer.CustomerRequest request = new RegisterCustomer.CustomerRequest(id, name, create_at, update_at);
+
+
+        registerCustomer.register(request);
+
     }
 
     private class RegisterCustomer {
+
+        public void register(final CustomerRequest customerRequest) {
+
+        }
+
+        public record CustomerRequest(Long id, String name, LocalDateTime create_at, LocalDateTime update_at) {
+            public CustomerRequest {
+                Assert.notNull(id, "아이디는 필수입니다.");
+                Assert.hasText(name, "고객 이름은 필수입니다.");
+                Assert.notNull(create_at, " 생성 시간은 필수입니다.");
+                Assert.notNull(update_at, " 수정 시간은 필수입니다.");
+            }
+        }
     }
 }
