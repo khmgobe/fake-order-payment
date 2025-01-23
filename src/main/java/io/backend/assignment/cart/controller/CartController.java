cf2a4968a029f4f976ce38881e59127f1d1e2d1d
khmgobe <dr94406@naver.com>
2025-01-23 22:40:41 +0900

    17. feat: 장바구니 수정 Api 작성.

--- src/main/java/io/backend/assignment/cart/controller/CartController.java
+++ src/main/java/io/backend/assignment/cart/controller/CartController.java
@@ -1,5 +1,6 @@
 package io.backend.assignment.cart.controller;
 
+import io.backend.assignment.cart.controller.dto.request.UpdateCartRequest;
 import io.backend.assignment.cart.controller.dto.response.GetCartResponse;
 import io.backend.assignment.cart.service.usecase.CartServiceUseCase;
 import io.backend.assignment.cart.controller.dto.request.RegisterCartRequest;
@@ -16,7 +17,7 @@ class CartController {
     private final CartServiceUseCase cartServiceUseCase;
 
     @PostMapping("/api/v1/carts/{productId}/{customerId}")
-    public ResponseEntity<Void> register(
+    public ResponseEntity<Void> registerCart(
             @PathVariable final Long productId,
             @PathVariable final Long customerId,
             @RequestBody @Valid final RegisterCartRequest request) {
@@ -26,12 +27,20 @@ class CartController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("api/v1/carts/{cartId}")
+    @GetMapping("/api/v1/carts/{cartId}")
     public ResponseEntity<GetCartResponse> getCart(@PathVariable final Long cartId) {
 
         final GetCartResponse response = cartServiceUseCase.getCart(cartId);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/api/v1/carts/{cartId}")
+    public ResponseEntity<Void> updateCart (@PathVariable final Long cartId, @RequestBody @Valid final UpdateCartRequest request) {
+
+        cartServiceUseCase.updateCart(cartId, request);
+
+        return ResponseEntity.ok().build();
     }
 }
 
