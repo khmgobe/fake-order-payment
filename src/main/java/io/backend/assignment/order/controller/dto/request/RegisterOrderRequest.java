52a78b7f141b5ee9fc8520640f82c18f84205901
khmgobe <dr94406@naver.com>
2025-01-24 19:31:33 +0900

    5. feat: OrderRequest, OrderResponse  record 생성.

---
+++ src/main/java/io/backend/assignment/order/controller/dto/request/RegisterOrderRequest.java
@@ -0,0 +1,33 @@
+package io.backend.assignment.order.controller.dto.request;
+
+import io.backend.assignment.cart.domain.Cart;
+import io.backend.assignment.customer.domain.Customer;
+import io.backend.assignment.order.domain.Order;
+import io.backend.assignment.order.domain.enumeration.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record RegisterOrderRequest(
+
+        @NotNull(message = "주문상태는 필수입니다.")
+        OrderStatus orderStatus,
+        @NotNull(message = "주문 생성 시간은 필수입니다.")
+        LocalDateTime createdAt,
+        @NotNull(message = "주문 수정 시간은 필수입니다.")
+        LocalDateTime updatedAt) {
+
+    public Order toDomain(final Customer customer, final List<Cart> carts, final Long totalAmount) {
+
+        return Order
+                .builder()
+                .customer(customer)
+                .carts(carts)
+                .totalAmount(totalAmount)
+                .orderStatus(OrderStatus.PENDING)
+                .createdAt(createdAt())
+                .updatedAt(updatedAt())
+                .build();
+    }
+}
