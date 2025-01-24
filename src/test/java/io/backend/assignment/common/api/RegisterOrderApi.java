baafd7db770ca25c0c74a72995b80b76daf6b843
khmgobe <dr94406@naver.com>
2025-01-24 19:29:35 +0900

    3. feat: 시나리오 테스트를 위한 RegisterOrderApi 추가.

---
+++ src/test/java/io/backend/assignment/common/api/RegisterOrderApi.java
@@ -0,0 +1,37 @@
+package io.backend.assignment.common.api;
+
+import io.backend.assignment.common.TestScenario;
+import io.backend.assignment.order.controller.dto.request.RegisterOrderRequest;
+import io.backend.assignment.order.domain.enumeration.OrderStatus;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class RegisterOrderApi {
+
+    private Long customerId = 1L;
+    private OrderStatus orderStatus = OrderStatus.PENDING;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public ValidatableResponse register() {
+
+        TestScenario.registerCartApi().register();
+
+        final RegisterOrderRequest request = new RegisterOrderRequest(orderStatus, createdAt, updatedAt);
+
+        final ValidatableResponse response = RestAssured.given().log().all()
+                .when()
+                .body(request)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/v1/orders/{customerId}", customerId)
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        return response;
+    }
+}
