e0f77fa47b4f30d030620b2d83ff848957947c1e
khmgobe <dr94406@naver.com>
2025-01-24 19:28:57 +0900

    2. feat: 사용자 아이디로 장바구니를 찾을 수 있는 메서드 추가.

--- src/main/java/io/backend/assignment/cart/repository/CartRepository.java
+++ src/main/java/io/backend/assignment/cart/repository/CartRepository.java
@@ -3,5 +3,8 @@ package io.backend.assignment.cart.repository;
 import io.backend.assignment.cart.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByCustomerId(Long customerId);
 }
