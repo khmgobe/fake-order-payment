8149ebb1f07c439a07c6876ff58dd8f86c47c168
khmgobe <dr94406@naver.com>
2025-01-26 02:04:11 +0900

    8. feat: 주문 -> 결제 -> 결제 성공/실패에 따른 이력을 남기는 기능 추가 및 테스트.

--- src/test/java/io/backend/assignment/payment/PaymentServiceTest.java
+++ src/test/java/io/backend/assignment/payment/PaymentServiceTest.java
@@ -10,8 +10,10 @@ import io.backend.assignment.order.controller.dto.request.RegisterOrderRequest;
 import io.backend.assignment.order.controller.dto.response.GetOrderResponse;
 import io.backend.assignment.order.domain.enumeration.OrderStatus;
 import io.backend.assignment.order.service.usecase.OrderService;
-import io.backend.assignment.payment.dto.response.PaymentResponse;
-import io.backend.assignment.payment.enumeration.PaymentStatus;
+import io.backend.assignment.payment.domain.dto.response.PaymentHistoryResponse;
+import io.backend.assignment.payment.domain.dto.response.PaymentResponse;
+import io.backend.assignment.payment.domain.enumeration.PaymentStatus;
+import io.backend.assignment.payment.service.PaymentHistoryService;
 import io.backend.assignment.payment.service.PaymentService;
 import io.backend.assignment.product.ProductSteps;
 import io.backend.assignment.product.controller.request.ProductRequest;
@@ -42,6 +44,9 @@ class PaymentServiceTest {
     @Autowired
     private PaymentService paymentService;
 
+    @Autowired
+    private PaymentHistoryService paymentHistoryService;
+
     @BeforeEach
     void setUp() {
         registerCustomer();
@@ -50,11 +55,12 @@ class PaymentServiceTest {
     }
 
     @Test
-    @DisplayName("주문을 생성하고 결제를 처리한다. [정상 케이스]")
+    @DisplayName("주문을 생성하고 결제한다. 결제 이후에는 결제 이력이 남아야 한다.[정상 케이스]")
     void createOrderAndProcessPayment() {
 
         final Long customerId = 1L;
         final Long orderId = 1L;
+        final Long paymentHistoryId = 1L;
 
         // given : 주문 요청에 필요한 데이터를 만들고 주문을 요청한다.
         final RegisterOrderRequest registerOrderRequest = OrderSteps.orderRequest();
@@ -63,12 +69,16 @@ class PaymentServiceTest {
         // when : 결제 요청을 처리한다.
         final PaymentResponse paymentResponse = paymentService.payment(orderId);
 
-        // then : 결제가 정상적으로 처리되었는지 확인하고 결제 후 변동된 주문 상태를 확인한다.
+        // then : 결제가 정상적으로 처리되었는지 확인하고 결제 후 변동된 주문 상태와 결제 이력을 확인한다.
         assertThat(paymentResponse.status()).isEqualTo(PaymentStatus.SUCCESS.name());
         assertThat(paymentResponse.message()).isEqualTo("Payment processed successfully");
 
         final GetOrderResponse afterOrderResponse = orderService.getOrder(orderId);
         assertThat(afterOrderResponse.orderStatus()).isEqualTo(OrderStatus.COMPLETE);
+
+        final PaymentHistoryResponse paymentHistoryResponse = paymentHistoryService.getBy(paymentHistoryId);
+        assertThat(paymentHistoryResponse.status()).isEqualTo(PaymentStatus.SUCCESS.name());
+        assertThat(paymentHistoryResponse.amount()).isEqualTo(orderResponse.totalAmount());
     }
 
     private void registerCustomer() {
